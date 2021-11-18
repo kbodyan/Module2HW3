d605ff911488efd63168375f6e5e4b8cf7899d6e
kbodyan <kbodyan@gmail.com>
2021-11-18 17:24:02 +0200

    empty project

---
+++ Sweets/Sweets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sweets
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var start = new Starter();
+            start.Run();
+        }
+    }
+}
